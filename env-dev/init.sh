092d001c90bdfb6ae663b1bd5c1f7a27dc385873
kendall libby <klibby@mozilla.com>
2016-05-04 17:00:58 -0400

    add bare env dirs

---
+++ env-dev/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 
+set -euf -o pipefail
+
+tfenv="dev"
+
+# Set up remote state
+terraform remote config -backend=s3 \
+    -backend-config="bucket=moz-mozreview-state" \
+    -backend-config="key=${tfenv}/terraform.tfstate" \
+    -backend-config="region=us-west-2"
+
+# Update modules
+terraform get
