a80d82909c10485d9f4c8060f0ec2931913aae59
kendall libby <klibby@mozilla.com>
2016-05-04 12:19:17 -0400

    create sns topic for remote state bucket notifications

--- main.tf
+++ main.tf
@@ -61,6 +61,33 @@ resource "aws_s3_bucket" "tf_state-bucket" {
     }
 }
 
+# Create an SNS topic for tf_state-bucket notifications
+variable "tf_sns_topic_name" {
+    description = "Hack to work around GH-4157"
+    default = "tf_state_notifications"
+}
+resource "template_file" "bucket_sns-policy" {
+    template = "${file("bucket_sns.json.tmpl")}"
+    vars {
+        region = "${var.region}"
+        account_id = "${var.account_id}"
+        topic_name = "${var.tf_sns_topic_name}"
+        bucket_name = "${var.tf_state_bucket}"
+    }
+}
+resource "aws_sns_topic" "tf_state_bucket-topic" {
+    name = "${var.tf_sns_topic_name}"
+    display_name = "tf-state"
+    policy = "${template_file.bucket_sns-policy.rendered}"
+}
+# email protocol not supported, as it requires out-of-band authorization
+#resource "aws_sns_topic_subscription" "bucket_writes_sub" {
+#    topic_arn = "${aws_sns_topic.bucket_writes.arn}"
+#    protocol = "email"
+#    endpoint = "..."
+#}
+
+
 output "tf_state_bucket_arn" {
     value = "${aws_s3_bucket.tf_state-bucket.arn}"
 }
