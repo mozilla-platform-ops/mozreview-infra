0c494e61e40b8f9a9c0f51e9fb1ff8b87f96e19c
kendall libby <klibby@mozilla.com>
2016-05-04 14:35:34 -0400

    add use notes to each file

--- variables.tf
+++ variables.tf
@@ -1,11 +1,15 @@
+# This file contains default variabled that are shared by all environments
+
 variable "account_id" {
     description = "AWS Account ID"
     default = "154007893214"
 }
+
 variable "tf_state_bucket" {
     description = "Name of the S3 bucket for Terraform remote state"
     default = "moz-mozreview-state"
 }
+
 variable "cloudtrail_bucket" {
     description = "Name of the S3 bucket for local cloudtrail logging"
     default = "moz-mozreview-logging"
