3bb5a3346b6d4c4b86052c15b8bc266d7d3406b8
kendall libby <klibby@mozilla.com>
2016-05-26 15:24:27 -0700

    fix missing brackets

--- modules/tf_hg/outputs.tf
+++ modules/tf_hg/outputs.tf
@@ -2,11 +2,11 @@ output "hg_eip" {
     value = "${aws_eip.eip.public_ip}"
 }
 output "hg_elb_id" {
-    value = "${aws_elb.elb.id"
+    value = "${aws_elb.elb.id}"
 }
 output "hg_elb_name" {
-    value = "${aws_elb.elb.name"
+    value = "${aws_elb.elb.name}"
 }
 output "hg_elb_dns" {
-    value = "${aws_elb.elb.dns_name"
+    value = "${aws_elb.elb.dns_name}"
 }
