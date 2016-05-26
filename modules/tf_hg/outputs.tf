db0b8dfe4a0edc93e9d1abeef7004edb7452481f
kendall libby <klibby@mozilla.com>
2016-05-26 15:23:56 -0700

    add elb outputs

--- modules/tf_hg/outputs.tf
+++ modules/tf_hg/outputs.tf
@@ -1,3 +1,12 @@
 output "hg_eip" {
     value = "${aws_eip.eip.public_ip}"
 }
+output "hg_elb_id" {
+    value = "${aws_elb.elb.id"
+}
+output "hg_elb_name" {
+    value = "${aws_elb.elb.name"
+}
+output "hg_elb_dns" {
+    value = "${aws_elb.elb.dns_name"
+}
