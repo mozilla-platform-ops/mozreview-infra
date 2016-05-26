b2b3d494ff78731a9fec8e24363460c7223d3933
kendall libby <klibby@mozilla.com>
2016-05-26 15:19:09 -0700

    remove prefix option

--- modules/tf_hg/ec2.tf
+++ modules/tf_hg/ec2.tf
@@ -38,3 +38,38 @@ resource "aws_volume_attachment" "attach" {
     volume_id = "${aws_ebs_volume.vol.id}"
     instance_id = "${aws_instance.hg.id}"
 }
+
+resource "aws_elb" "elb" {
+    name = "hg-elb"
+    instances = ["${aws_instance.hg.id}"]
+    security_groups = [""]
+    subnets = ["${var.subnet_id}"]
+    
+    access_logs {
+        bucket = "${var.logging_bucket}"
+        interval = 60
+    }
+    listener {
+        instance_port = 80
+        instance_protocol = "http"
+        lb_port = 80
+        lb_protocol = "http"
+    }
+    #listener {
+    #    instance_port = 443
+    #    instance_protocol = "https"
+    #    lb_port = 443
+    #    lb_protocol = "https"
+    #    #ssl_certificate_id = "arn:..."
+    #}
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        target = "HTTP:80/"
+        interval = 3
+    }
+    tags {
+        Name = "hg-elb"
+    }
+}
